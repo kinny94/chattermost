dfd765fa8f3f86365ccf6f59294e2fe3e984bd44
kinny94 <ad.sam007@gmail.com>
2018-12-14 00:17:22 -0500

    fix: working on crreate-chatroom

--- src/app/components/chatrooms/create-chatroom/create-chatroom/create-chatroom.component.ts
+++ src/app/components/chatrooms/create-chatroom/create-chatroom/create-chatroom.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {User} from './../../../../models/user-model'
+import { UserService } from 'src/app/services/user-service/user.service';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
+import { AngularFireList } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-chatroom',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateChatroomComponent implements OnInit {
 
-  constructor() { }
+  chatroom_users$: Observable<User[]>
+  moderators$: Observable<User[]>
+  datasoure$: AngularFireList<User>
+  currentUser: User;
+
+
+  constructor(private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getAppUser$().subscribe(user => this.currentUser = user)
+    this.datasoure$ = this.userService.getAllUsers();
+    this.moderators$.pipe(
+      map(users => users.push(this.currentUser))
+    )
+    this.chatroom_users$.pipe(
+      map(users => users.push(this.currentUser))
+    )
+
+
+    this.chatroom_users$.subscribe(console.log);
+    // this.chatroom_users$.push(this.currentUser);
+    this.datasoure$.valueChanges().subscribe(console.log);
   }
 
 }
